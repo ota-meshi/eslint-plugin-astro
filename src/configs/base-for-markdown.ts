d5f20139b833183e0fdec10e707b70e2944a8da8
yosuke ota <otameshiyo23@gmail.com>
2022-08-10 15:40:48 +0900

    0.17.0

---
+++ src/configs/base-for-markdown.ts
@@ -0,0 +1,37 @@
+import { hasTypescriptEslintParser } from "./has-typescript-eslint-parser"
+
+export = {
+  plugins: ["astro"],
+  overrides: [
+    {
+      // Define the configuration for `.md` file.
+      files: ["*.md"],
+      // Enable this plugin
+      plugins: ["astro"],
+      env: {
+        // Enables global variables available in Astro Markdown pages.
+        node: true,
+        "astro/markdown": true,
+        es2020: true,
+      },
+      // Allows Astro Markdown pages to be parsed.
+      parser: require.resolve("astro-eslint-parser"),
+      // Parse the script in `.md` as TypeScript by adding the following configuration.
+      parserOptions: {
+        parser: hasTypescriptEslintParser
+          ? "@typescript-eslint/parser"
+          : undefined,
+        extraFileExtensions: [".md"],
+        // The script of Astro Markdown pages uses ESM.
+        sourceType: "module",
+      },
+      rules: {
+        // Turn off rules that are incompatible with markdown
+        indent: "off",
+        // eslint-plugin-astro rules
+        // Enable base rules
+        
+      },
+    },
+  ],
+}
