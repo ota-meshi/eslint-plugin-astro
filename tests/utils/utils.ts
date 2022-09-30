5ea28724753221c65c5266875642244fd71ac558
Yosuke Ota <otameshiyo23@gmail.com>
2022-09-30 21:06:21 +0900

    Update utils.ts

--- tests/utils/utils.ts
+++ tests/utils/utils.ts
@@ -93,7 +93,7 @@ export function loadTestCases(
       let errors
       try {
         errors = fs.readFileSync(errorFile, "utf8")
-      } catch (e) {
+      } catch (_e) {
         writeFixtures(ruleName, inputFile)
         errors = fs.readFileSync(errorFile, "utf8")
       }
@@ -102,7 +102,7 @@ export function loadTestCases(
         let output
         try {
           output = fs.readFileSync(outputFile, "utf8")
-        } catch (e) {
+        } catch (_e) {
           writeFixtures(ruleName, inputFile)
           output = fs.readFileSync(outputFile, "utf8")
         }
