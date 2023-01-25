0d882adb9a3121dadb7829fc147351238573370b
yosuke ota <otameshiyo23@gmail.com>
2023-01-25 09:26:14 +0900

    docs: fix source

--- docs-build/setup-docs.mjs
+++ docs-build/setup-docs.mjs
@@ -52,9 +52,12 @@ ${dump(data)}---
 
 
 
-<a href="https://github.com/ota-meshi/eslint-plugin-astro/edit/main/docs/${page.slice(
-      docsDir.length,
-    )}" target="_blank" style="display: flex; color: var(--theme-text-lighter);">Edit this page <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" x="0px" y="0px" viewBox="0 0 100 100" width="15" height="15" class="icon outbound"><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path> <polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg></a>`
+<a href="https://github.com/ota-meshi/eslint-plugin-astro/edit/main/docs/${page
+      .slice(docsDir.length)
+      .replace(
+        /^\//u,
+        "",
+      )}" target="_blank" style="display: flex; color: var(--theme-text-lighter);">Edit this page <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" x="0px" y="0px" viewBox="0 0 100 100" width="15" height="15" class="icon outbound"><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path> <polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg></a>`
 
     // Remove comments
     pageContent = pageContent.replace(/<!--[\s\S]*?-->/g, "")
---
+++ docs-build/src/env.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="astro/client" />
+/* eslint spaced-comment: 0 -- ignore */
