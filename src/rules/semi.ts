8e670ccac278113eeb137a082be376ef6fdc6055
ota-meshi <otameshiyo23@gmail.com>
2023-05-08 13:47:16 +0900

    fix: incompatible with ESLint v8.40 in `astro/semi` rule

--- src/rules/semi.ts
+++ src/rules/semi.ts
@@ -27,85 +27,86 @@ export default createRule("semi", {
 
     return coreRule.create(
       newProxy(context, {
-        getSourceCode() {
-          if (sourceCodeWrapper) {
-            return sourceCodeWrapper
-          }
-          const sourceCode = context.getSourceCode()
-
-          /** Transforms token */
-          function transformToken(
-            token: AST.Token | AST.Comment,
-          ): AST.Token | AST.Comment {
-            return token.value === "---"
-              ? newProxy(token, { value: "___" })
-              : token
-          }
-
-          return (sourceCodeWrapper = newProxy(sourceCode, {
-            /* eslint-disable @typescript-eslint/unbound-method -- ignore */
-            getFirstToken: wrapGetTokenFunction(sourceCode.getFirstToken),
-            getFirstTokens: wrapGetTokensFunction(sourceCode.getFirstTokens),
-            getFirstTokenBetween: wrapGetTokenFunction(
-              sourceCode.getFirstTokenBetween,
-            ),
-            getFirstTokensBetween: wrapGetTokensFunction(
-              sourceCode.getFirstTokensBetween,
-            ),
-            getLastToken: wrapGetTokenFunction(sourceCode.getLastToken),
-            getLastTokens: wrapGetTokensFunction(sourceCode.getLastTokens),
-            getLastTokenBetween: wrapGetTokenFunction(
-              sourceCode.getLastTokenBetween,
-            ),
-            getLastTokensBetween: wrapGetTokensFunction(
-              sourceCode.getLastTokensBetween,
-            ),
-            getTokenBefore: wrapGetTokenFunction(sourceCode.getTokenBefore),
-            getTokensBefore: wrapGetTokensFunction(sourceCode.getTokensBefore),
-            getTokenAfter: wrapGetTokenFunction(sourceCode.getTokenAfter),
-            getTokensAfter: wrapGetTokensFunction(sourceCode.getTokensAfter),
-            getTokenByRangeStart: wrapGetTokenFunction(
-              sourceCode.getTokenByRangeStart,
-            ),
-            getTokens: wrapGetTokensFunction(sourceCode.getTokens),
-            getTokensBetween: wrapGetTokensFunction(
-              sourceCode.getTokensBetween,
-            ),
-            /* eslint-enable @typescript-eslint/unbound-method -- ignore */
-          }))
-
-          /** Wrap token getter function */
-          function wrapGetTokenFunction<
-            T extends (
-              this: SourceCode,
-              ...args: never[]
-            ) => AST.Token | AST.Comment | null,
-          >(base: T): T {
-            return function (this: SourceCode, ...args) {
-              // eslint-disable-next-line no-invalid-this -- is valid
-              const token = base.apply(this, args)
-              if (!token) {
-                return token
-              }
-              return transformToken(token)
-            } as T
-          }
-
-          /** Wrap tokens getter function */
-          function wrapGetTokensFunction<
-            T extends (
-              this: SourceCode,
-              ...args: never[]
-            ) => (AST.Token | AST.Comment)[],
-          >(base: T): T {
-            return function (this: SourceCode, ...args) {
-              // eslint-disable-next-line no-invalid-this -- is valid
-              const tokens = base.apply(this, args)
-              return tokens.map(transformToken)
-            } as T
-          }
+        getSourceCode,
+        get sourceCode() {
+          return getSourceCode()
         },
       }),
     )
+
+    function getSourceCode() {
+      if (sourceCodeWrapper) {
+        return sourceCodeWrapper
+      }
+      const sourceCode = context.getSourceCode()
+
+      /** Transforms token */
+      function transformToken(
+        token: AST.Token | AST.Comment,
+      ): AST.Token | AST.Comment {
+        return token.value === "---" ? newProxy(token, { value: "___" }) : token
+      }
+
+      return (sourceCodeWrapper = newProxy(sourceCode, {
+        /* eslint-disable @typescript-eslint/unbound-method -- ignore */
+        getFirstToken: wrapGetTokenFunction(sourceCode.getFirstToken),
+        getFirstTokens: wrapGetTokensFunction(sourceCode.getFirstTokens),
+        getFirstTokenBetween: wrapGetTokenFunction(
+          sourceCode.getFirstTokenBetween,
+        ),
+        getFirstTokensBetween: wrapGetTokensFunction(
+          sourceCode.getFirstTokensBetween,
+        ),
+        getLastToken: wrapGetTokenFunction(sourceCode.getLastToken),
+        getLastTokens: wrapGetTokensFunction(sourceCode.getLastTokens),
+        getLastTokenBetween: wrapGetTokenFunction(
+          sourceCode.getLastTokenBetween,
+        ),
+        getLastTokensBetween: wrapGetTokensFunction(
+          sourceCode.getLastTokensBetween,
+        ),
+        getTokenBefore: wrapGetTokenFunction(sourceCode.getTokenBefore),
+        getTokensBefore: wrapGetTokensFunction(sourceCode.getTokensBefore),
+        getTokenAfter: wrapGetTokenFunction(sourceCode.getTokenAfter),
+        getTokensAfter: wrapGetTokensFunction(sourceCode.getTokensAfter),
+        getTokenByRangeStart: wrapGetTokenFunction(
+          sourceCode.getTokenByRangeStart,
+        ),
+        getTokens: wrapGetTokensFunction(sourceCode.getTokens),
+        getTokensBetween: wrapGetTokensFunction(sourceCode.getTokensBetween),
+        /* eslint-enable @typescript-eslint/unbound-method -- ignore */
+      }))
+
+      /** Wrap token getter function */
+      function wrapGetTokenFunction<
+        T extends (
+          this: SourceCode,
+          ...args: never[]
+        ) => AST.Token | AST.Comment | null,
+      >(base: T): T {
+        return function (this: SourceCode, ...args) {
+          // eslint-disable-next-line no-invalid-this -- is valid
+          const token = base.apply(this, args)
+          if (!token) {
+            return token
+          }
+          return transformToken(token)
+        } as T
+      }
+
+      /** Wrap tokens getter function */
+      function wrapGetTokensFunction<
+        T extends (
+          this: SourceCode,
+          ...args: never[]
+        ) => (AST.Token | AST.Comment)[],
+      >(base: T): T {
+        return function (this: SourceCode, ...args) {
+          // eslint-disable-next-line no-invalid-this -- is valid
+          const tokens = base.apply(this, args)
+          return tokens.map(transformToken)
+        } as T
+      }
+    }
   },
 })
