5ad4b47042c5d1b803e30ac2f9eb820a3918bb44
ota-meshi <otameshiyo23@gmail.com>
2023-05-09 10:41:58 +0900

    test: fix

--- tests/src/meta.ts
+++ tests/src/meta.ts
@@ -8,13 +8,16 @@ const expectedMeta = {
 
 describe("Test for meta object", () => {
   it("A plugin should have a meta object.", () => {
-    assert.deepStrictEqual(plugin.meta, expectedMeta)
+    assert.strictEqual(plugin.meta.name, expectedMeta.name)
+    assert.strictEqual(typeof plugin.meta.version, "string")
   })
 
   for (const [name, processor] of Object.entries(plugin.processors)) {
     it(`"${name}" processor should have a meta object.`, () => {
       // @ts-expect-error -- missing type
-      assert.deepStrictEqual(processor.meta, expectedMeta)
+      assert.strictEqual(processor.meta.name, expectedMeta.name)
+      // @ts-expect-error -- missing type
+      assert.strictEqual(typeof processor.meta.version, "string")
     })
   }
 })
