84ab5001b8c825fd003034c402a5b370c0e6216c
Yosuke Ota <otameshiyo23@gmail.com>
2023-07-15 20:34:12 +0000

    fix

--- docs-build/build-system/src/eslint-plugin-jsx-a11y.mjs
+++ docs-build/build-system/src/eslint-plugin-jsx-a11y.mjs
@@ -1,5 +1,6 @@
 /* eslint require-jsdoc:0 -- shim */
 
+// eslint-disable-next-line n/no-missing-import -- Ignore
 import * as all from "../../../node_modules/eslint-plugin-jsx-a11y"
 export const rules = all.rules
 export default all
