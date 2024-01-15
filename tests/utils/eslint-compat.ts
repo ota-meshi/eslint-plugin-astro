879015fe6f87c8795f38a1acf9e2409a252b4d3f
ota-meshi <otameshiyo23@gmail.com>
2024-01-15 17:41:39 +0900

    fix

--- tests/utils/eslint-compat.ts
+++ tests/utils/eslint-compat.ts
@@ -1,8 +1,16 @@
 import { ESLint as OriginalESLint } from "eslint"
-import * as unsupported from "eslint/use-at-your-own-risk"
 import { getRuleTester } from "eslint-compat-utils/rule-tester"
 
 // eslint-disable-next-line @typescript-eslint/naming-convention -- class name
-export const ESLint = (unsupported as any).LegacyESLint || OriginalESLint
+export const ESLint = getUnsupported().LegacyESLint || OriginalESLint
 // eslint-disable-next-line @typescript-eslint/naming-convention -- class name
 export const RuleTester = getRuleTester()
+
+function getUnsupported() {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports -- ignore
+    return require("eslint/use-at-your-own-risk")
+  } catch {
+    return {}
+  }
+}
