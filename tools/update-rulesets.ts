10b17aafec9ea99a3d31da02a62dd1bdeeeb5af5
ota-meshi <otameshiyo23@gmail.com>
2024-04-26 16:55:13 +0900

    fix: crash in esm

--- tools/update-rulesets.ts
+++ tools/update-rulesets.ts
@@ -12,83 +12,89 @@ void formatAndSave(
  * This file has been automatically generated,
  * in order to update its content execute "npm run update"
  */
+import type { Linter } from "eslint"
 import { hasTypescriptEslintParser } from "./has-typescript-eslint-parser"
 
-export default {
-  plugins: ["astro"],
-  overrides: [
-    {
-      // Define the configuration for \`.astro\` file.
-      files: ["*.astro"],
-      // Enable this plugin
-      plugins: ["astro"],
-      env: {
-        // Enables global variables available in Astro components.
-        node: true,
-        "astro/astro": true,
-        es2020: true,
+/**
+ * Build legacy base config
+ */
+export function buildLegacyBase(): Linter.Config {
+  return {
+    plugins: ["astro"],
+    overrides: [
+      {
+        // Define the configuration for \`.astro\` file.
+        files: ["*.astro"],
+        // Enable this plugin
+        plugins: ["astro"],
+        env: {
+          // Enables global variables available in Astro components.
+          node: true,
+          "astro/astro": true,
+          es2020: true,
+        },
+        // Allows Astro components to be parsed.
+        parser: require.resolve("astro-eslint-parser"),
+        // Parse the script in \`.astro\` as TypeScript by adding the following configuration.
+        parserOptions: {
+          parser: hasTypescriptEslintParser
+            ? "@typescript-eslint/parser"
+            : undefined,
+          extraFileExtensions: [".astro"],
+          // The script of Astro components uses ESM.
+          sourceType: "module",
+        },
+        rules: {
+          // eslint-plugin-astro rules
+          // Enable base rules
+          ${baseRules
+            .map((rule) => {
+              const conf = rule.meta.docs.default || "error"
+              return `"${rule.meta.docs.ruleId}": "${conf}"`
+            })
+            .join(",\n        ")}
+        },
       },
-      // Allows Astro components to be parsed.
-      parser: require.resolve("astro-eslint-parser"),
-      // Parse the script in \`.astro\` as TypeScript by adding the following configuration.
-      parserOptions: {
+      {
+        // Define the configuration for \`<script>\` tag.
+        // Script in \`<script>\` is assigned a virtual file name with the \`.js\` extension.
+        files: ["**/*.astro/*.js", "*.astro/*.js"],
+        env: {
+          browser: true,
+          es2020: true,
+        },
+        parserOptions: {
+          sourceType: "module",
+        },
+        rules: {
+          // If you are using "prettier/prettier" rule,
+          // you don't need to format inside <script> as it will be formatted as a \`.astro\` file.
+          "prettier/prettier": "off",
+        },
+      },
+      {
+        // Define the configuration for \`<script>\` tag when using \`client-side-ts\` processor.
+        // Script in \`<script>\` is assigned a virtual file name with the \`.ts\` extension.
+        files: ["**/*.astro/*.ts", "*.astro/*.ts"],
+        env: {
+          browser: true,
+          es2020: true,
+        },
         parser: hasTypescriptEslintParser
           ? "@typescript-eslint/parser"
           : undefined,
-        extraFileExtensions: [".astro"],
-        // The script of Astro components uses ESM.
-        sourceType: "module",
+        parserOptions: {
+          sourceType: "module",
+          project: null,
+        },
+        rules: {
+          // If you are using "prettier/prettier" rule,
+          // you don't need to format inside <script> as it will be formatted as a \`.astro\` file.
+          "prettier/prettier": "off",
+        },
       },
-      rules: {
-        // eslint-plugin-astro rules
-        // Enable base rules
-        ${baseRules
-          .map((rule) => {
-            const conf = rule.meta.docs.default || "error"
-            return `"${rule.meta.docs.ruleId}": "${conf}"`
-          })
-          .join(",\n        ")}
-      },
-    },
-    {
-      // Define the configuration for \`<script>\` tag.
-      // Script in \`<script>\` is assigned a virtual file name with the \`.js\` extension.
-      files: ["**/*.astro/*.js", "*.astro/*.js"],
-      env: {
-        browser: true,
-        es2020: true,
-      },
-      parserOptions: {
-        sourceType: "module",
-      },
-      rules: {
-        // If you are using "prettier/prettier" rule,
-        // you don't need to format inside <script> as it will be formatted as a \`.astro\` file.
-        "prettier/prettier": "off",
-      },
-    },
-    {
-      // Define the configuration for \`<script>\` tag when using \`client-side-ts\` processor.
-      // Script in \`<script>\` is assigned a virtual file name with the \`.ts\` extension.
-      files: ["**/*.astro/*.ts", "*.astro/*.ts"],
-      env: {
-        browser: true,
-        es2020: true,
-      },
-      parser: hasTypescriptEslintParser
-        ? "@typescript-eslint/parser"
-        : undefined,
-      parserOptions: {
-        sourceType: "module",
-        project: null,
-      },
-      rules: {
-        // If you are using "prettier/prettier" rule,
-        // you don't need to format inside <script> as it will be formatted as a \`.astro\` file.
-        "prettier/prettier": "off",
-      },
-    },
-  ],
+    ],
+  }
 }
 `,
 )
