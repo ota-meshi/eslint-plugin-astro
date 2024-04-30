d77703bf0e3dbee83da8468cd426bb8fe5e464e2
jimmy-guzman <hi@jimmyguzman.com>
2024-04-30 06:51:59 -0500

    test: ✅ just test wether or not tsc throws

--- tests/integration/test.ts
+++ tests/integration/test.ts
@@ -67,19 +67,7 @@ for (const dirent of fs.readdirSync(TEST_FIXTURES_ROOT, {
     })
 
     it("should NOT fail when running tsc", () => {
-      try {
-        cp.execSync(`npx tsc`, { cwd: TEST_CWD })
-
-        assert.fail("Expect error")
-      } catch (e: any) {
-        const result = `${e.stdout}`
-        try {
-          assert.equal(result, "undefined")
-        } catch (e) {
-          console.error(e)
-          throw e
-        }
-      }
+      cp.execSync(`npx tsc`, { cwd: TEST_CWD })
     })
   })
 }
