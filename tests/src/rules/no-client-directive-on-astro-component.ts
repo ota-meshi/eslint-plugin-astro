82080bbcc28d0fed360ff95f89bb103b096f745f
Raisul Chowdhury <34920788+raisulchowdhury@users.noreply.github.com>
2026-08-13 06:05:45 -0700

    test: allow lowercase HTML tags with Astro imports

--- tests/src/rules/no-client-directive-on-astro-component.ts
+++ tests/src/rules/no-client-directive-on-astro-component.ts
@@ -1,4 +1,5 @@
 import { RuleTester } from "eslint"
+import * as astroESLintParser from "astro-eslint-parser"
 import rule from "../../../src/rules/no-client-directive-on-astro-component.ts"
 import { loadTestCases } from "../../utils/utils.ts"
 
@@ -12,5 +13,21 @@ const tester = new RuleTester({
 tester.run(
   "no-client-directive-on-astro-component",
   rule as any,
-  loadTestCases("no-client-directive-on-astro-component"),
+  loadTestCases("no-client-directive-on-astro-component", {
+    filter: () => true,
+    additionals: {
+      valid: [
+        {
+          filename: "lowercase-import.astro",
+          code: `---
+import dialog from "./Dialog.astro"
+---
+
+<dialog client:load>Native dialog</dialog>
+`,
+          languageOptions: { parser: astroESLintParser },
+        },
+      ],
+    },
+  }),
 )
